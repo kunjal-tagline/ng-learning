8474c330fee4a99c22ac875c62d1ddc56c370ff7
kunjal-tagline <kunjal.tagline@gmail.com>
2022-02-09 16:27:58 +0530

    inprogress reactive form

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
@@ -16,12 +16,22 @@ export class ReactiveFormComponent implements OnInit {
 
   public createReactiveForm() {
     this.reactiveForm = new FormGroup({
-      username: new FormControl('kunjal'),
-      password: new FormControl(null)
-    })
+      email: new FormControl(
+        '',
+        Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$')
+      ),
+      username: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(10),
+      ]),
+      password: new FormControl('', [
+        Validators.minLength(8),
+        Validators.pattern('(?=.*[0-9])'),
+      ]),
+    });
   }
   public reactiveFormSubmit() {
     console.log('reactiveloginfoorm data :>> ', this.reactiveForm);
-    }
   }
-
+}
