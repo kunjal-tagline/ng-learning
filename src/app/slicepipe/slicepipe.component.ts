7e76fedce745e760a47096b1a6c0e58244a4f18a
kunjal-tagline <kunjal.tagline@gmail.com>
2022-02-14 17:42:59 +0530

    slice pipe implement

--- src/app/slicepipe/slicepipe.component.ts
+++ src/app/slicepipe/slicepipe.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./slicepipe.component.scss']
 })
 export class SlicepipeComponent implements OnInit {
-
+  public sliceArray:Array<String>=['apple','bannana','cherry','Dargon fruit'];
+  public sliceString:string='hello everyone welcome in pipe session';
+  
   constructor() { }
 
   ngOnInit(): void {
