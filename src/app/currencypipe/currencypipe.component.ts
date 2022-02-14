68416fe835641b973cceaf0bf02499d3584299e9
kunjal-tagline <kunjal.tagline@gmail.com>
2022-02-14 16:01:28 +0530

    built in currency pipe

--- src/app/currencypipe/currencypipe.component.ts
+++ src/app/currencypipe/currencypipe.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-currencypipe',
   templateUrl: './currencypipe.component.html',
@@ -7,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CurrencypipeComponent implements OnInit {
 
+  public value:number=100000;
+  public value2:number=123.456;
+  public arrayvalue:Array<number>=[100,345,123,90];
   constructor() { }
 
   ngOnInit(): void {
