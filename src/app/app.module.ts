b8f2e8b9c5747e582b6d4add73634d98a18e3799
kunjal-tagline <kunjal.tagline@gmail.com>
2022-02-14 15:39:07 +0530

    custompipe for date

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DatepipeComponent } from './datepipe/datepipe.component';
+import { CustomdatePipe } from './customdate.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DatepipeComponent
+    DatepipeComponent,
+    CustomdatePipe
   ],
   imports: [
     BrowserModule,
---
+++ src/app/customdate.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { DatePipe } from '@angular/common';
+@Pipe({
+  name: 'customdate'
+})
+export class CustomdatePipe implements PipeTransform {
+
+  transform(date: Date | String,day:number, format:string='yyyy-MM-dd'): any{
+    date = new Date(); 
+    date.setDate(date.getDate()-day);
+    return new DatePipe('en-US').transform(date, format);
+  }
+
+}
