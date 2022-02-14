ec7a30464bfc656f23336bb714059284e6cd2c16
kunjal-tagline <kunjal.tagline@gmail.com>
2022-02-14 14:13:21 +0530

    component genrate for date pip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DatepipeComponent } from './datepipe/datepipe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DatepipeComponent
   ],
   imports: [
     BrowserModule,
