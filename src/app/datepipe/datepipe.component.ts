d894b2ce93a789025ec6b1875774ef69bc1e5b24
kunjal-tagline <kunjal.tagline@gmail.com>
2022-02-14 15:08:18 +0530

    built-in date pipe

--- src/app/datepipe/datepipe.component.ts
+++ src/app/datepipe/datepipe.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DatepipeComponent implements OnInit {
 
+public today:Date=new Date();
+
+
   constructor() { }
 
   ngOnInit(): void {
