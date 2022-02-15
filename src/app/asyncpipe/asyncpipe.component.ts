9aecd44e64f4d2d1a56aaa3f460a968da142cfb7
kunjal-tagline <kunjal.tagline@gmail.com>
2022-02-15 09:52:08 +0530

    async pipe impelement

--- src/app/asyncpipe/asyncpipe.component.ts
+++ src/app/asyncpipe/asyncpipe.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observer } from 'rxjs';
+import { Observable } from 'rxjs/internal/Observable';
+
 
 @Component({
   selector: 'app-asyncpipe',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./asyncpipe.component.scss']
 })
 export class AsyncpipeComponent implements OnInit {
-
-  constructor() { }
+  public promise: Promise<string> | undefined;
+  
+  constructor() { 
+    this.promise=this.getPromise();
+  }
 
   ngOnInit(): void {
   }
 
+  public time = new Observable<string>((observer: Observer<string>) => {
+    setInterval(() => observer.next(new Date().toString()), 1000);
+  });
+
+  public newNumber = new Observable<number>((observer: Observer<number>) => {
+    setInterval(() => observer.next(1234567890), 1000);
+  });
+
+  getPromise(): Promise<string> {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => resolve("Promise complete!"), 3000);
+    });
+ }
 }
